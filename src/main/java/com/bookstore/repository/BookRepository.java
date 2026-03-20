397a34ea8434c249a3bdf1c1e897ef21ffe43700
Nhân <thanhnhanqs2005@gmail.com>
2026-03-20 21:14:44 +0700

    Add sample data and update @EntityGraph for book entity

--- src/main/java/com/bookstore/repository/BookRepository.java
+++ src/main/java/com/bookstore/repository/BookRepository.java
@@ -1,12 +1,18 @@
 package com.bookstore.repository;
 
-
 import com.bookstore.entity.Book;
+import lombok.NonNull;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 
 @Repository
 public interface BookRepository extends JpaRepository<Book, Long> {
 
+    @EntityGraph(attributePaths = {"categories"})
+    @NonNull
+    @Override
+    List<Book> findAll();
 }
