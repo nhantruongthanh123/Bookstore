07d7c8e8b79241144be4dd1007d82dc78fd560f2
Nhân <thanhnhanqs2005@gmail.com>
2026-03-25 22:58:57 +0700

    Create sample role and AuthController

--- src/main/java/com/bookstore/controller/AuthController.java
+++ src/main/java/com/bookstore/controller/AuthController.java
@@ -1,7 +1,14 @@
 package com.bookstore.controller;
 
+import com.bookstore.dto.Auth.AuthResponse;
+import com.bookstore.dto.Auth.LoginRequest;
+import com.bookstore.dto.Auth.RegisterRequest;
 import com.bookstore.service.auth.AuthService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,4 +17,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("api/auth")
 public class AuthController {
     private final AuthService authService;
+
+    @PostMapping("/register")
+    public ResponseEntity<AuthResponse> register(@RequestBody @Valid RegisterRequest registerRequest) {
+        AuthResponse authResponse = authService.register(registerRequest);
+        return ResponseEntity.ok(authResponse);
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<AuthResponse> login(@RequestBody @Valid LoginRequest loginRequest) {
+        AuthResponse authResponse = authService.login(loginRequest);
+        return ResponseEntity.ok(authResponse);
+    }
 }
--- src/main/java/com/bookstore/security/SecurityConfig.java
+++ src/main/java/com/bookstore/security/SecurityConfig.java
@@ -69,7 +69,7 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
 
-        configuration.setAllowedOrigins(List.of("http://localhost:5173"));
+        configuration.setAllowedOriginPatterns(List.of("*"));
 
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
 
